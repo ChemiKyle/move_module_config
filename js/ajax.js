f8782a47d9047274efc57e700b241b11ddf0ca23
Kyle Chesney <kyle.2493@gmail.com>
2019-09-13 15:58:24 -0400

    expose fields which contain events so their values can be replaced before sending to source

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,6 @@
 $(function() {
     var selectedModulePrefix;
+    var selectedModuleConfigSchema;
     var selectedSourceProjectId;
     var selectedTargetProjectId;
     var moduleConfig;
@@ -13,7 +14,6 @@ $(function() {
             makeAjaxCall(selectedModulePrefix);
         });
 
-
     $sourceProjectsDropdown.change(function() {
         selectedSourceProjectId = $(this).children('option:selected')[0].getAttribute('value');
         makeAjaxCall(selectedModulePrefix, selectedSourceProjectId);
@@ -23,25 +23,29 @@ $(function() {
         selectedTargetProjectId = $(this).children('option:selected')[0].getAttribute('value');
         });
 
-    function makeAjaxCall(prefix = null, sourceProjectId = null, targetProjectId = null, transfer = null) {
+    function makeAjaxCall(prefix = null, sourceProjectId = null, targetProjectId = null, transfer = null, eventFields = null) {
         $.get(ajax_page,
                 {
                     ext_prefix: prefix,
                     source_project_id: sourceProjectId,
                     target_project_id: targetProjectId,
-                    transfer: transfer
+                    transfer: transfer,
+                    event_fields: eventFields
                 },
                 function(data) {
                     if (!transfer) {
                         if (prefix && !sourceProjectId) {
-                            //populateProjects(null, $targetProjectsDropdown);
+                            const responseData = JSON.parse(data);
                             sourceProjectOptions = [];
-                            for (project of JSON.parse(data)) {
+                            for (project of responseData.enabledProjects) {
                                 sourceProjectOptions.push(`<option value="${project.project_id}">${project.name}</option>`);
                             }
                             populateProjects(sourceProjectOptions, $sourceProjectsDropdown);
                             populateProjects(sourceProjectOptions, $targetProjectsDropdown);
-                        } else if (prefix && sourceProjectId) {
+                            selectedModuleConfigSchema = responseData.moduleConfigSchema;
+                        }
+                        else if (prefix && sourceProjectId) {
+                            //console.log(data);
                             var targetProjectOptions = $sourceProjectsDropdown
                                 .clone()
                                 .find('option')
@@ -51,7 +55,8 @@ $(function() {
                             populateProjects(targetProjectOptions, $targetProjectsDropdown);
                             moduleConfig = JSON.parse(data);
                         }
-                    } else {
+                    }
+                    else {
                         console.log(data);
                     }
 
@@ -60,7 +65,6 @@ $(function() {
          }
 
     $('#transfer-config').click(function() {
-
         // refresh variables
         selectedModulePrefix = $externalModulesDropdown.children('option:selected')[0].getAttribute('prefix');
         selectedSourceProjectId = $sourceProjectsDropdown.children('option:selected')[0].getAttribute('value');
@@ -73,7 +77,8 @@ $(function() {
                     return;
                 }
 
-        makeAjaxCall(selectedModulePrefix, selectedSourceProjectId, selectedTargetProjectId, true);
+        const eventFields = reportModuleEventFields(selectedModuleConfigSchema);
+        makeAjaxCall(selectedModulePrefix, selectedSourceProjectId, selectedTargetProjectId, transfer = true, eventFields);
 
         });
 
@@ -98,6 +103,36 @@ $(function() {
             element.click();
             document.body.removeChild(element);
     });
+
+    //TODO
+    $('#upload-settings').click(function() {
+        //const file = uploadFile();
+        //applyFile(file, targetProjectId);
+        return;
+    });
+
+    function reportModuleEventFields(moduleConfigSchema) {
+        // find fields which map to events so the event_ids may be changed to match the target project
+        //console.log(moduleConfigSchema['project-settings']);
+        let eventFields = [];
+
+        // must recursively search due to subsettings
+        // luckily setting them only needs their key
+        function eachRecursive(obj) {
+            for (var k in obj)
+            {
+                if (typeof obj[k] == "object" && obj[k] !== null) {
+                    eachRecursive(obj[k]);
+                } else if (k === 'type') {
+                    if (obj[k] === "event-list") {
+                        eventFields.push(obj.key);
+                    }
+                }
+            }
+        }
+        eachRecursive(moduleConfigSchema['project-settings']);
+        return eventFields;
+    }
 
     function populateProjects(projectOptions, $projectDropdown) {
         $projectDropdown.find('option').remove();
