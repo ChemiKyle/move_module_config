9392fe0af0232af89c8e2c69a5c4cdb814cc3e97
Kyle Chesney <kyle.2493@gmail.com>
2019-10-01 09:19:14 -0400

    add forgotten pages to git

---
+++ pages/project_copy_page.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
+
+$title = RCView::img(['src' => APP_PATH_IMAGES . 'bell.png']) . ' ' . REDCap::escapeHtml('Emily Page');
+echo RCView::h4([], $title);
+echo 'Hello World!!';
+
+require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
